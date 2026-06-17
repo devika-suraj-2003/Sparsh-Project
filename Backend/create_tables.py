017f0b5375da1310aa8bd7c2ce75b06757797259
gautham-977 <gauthamk0011@gmail.com>
2026-06-17 22:15:23 +0530

    Completed Project CRUD APIs

--- Backend/create_tables.py
+++ Backend/create_tables.py
@@ -3,6 +3,7 @@ from app.db.database import Base, engine
 # Import all models
 from app.models.role import Role
 from app.models.user import User
+from app.models.project import Project
 
 Base.metadata.create_all(bind=engine)
 
--- Backend/main.py
+++ Backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.user import router as user_router
+from app.api.project import router as project_router
 
 app = FastAPI()
 
@@ -14,6 +15,7 @@ app.add_middleware(
 )
 
 app.include_router(user_router)
+app.include_router(project_router)
 
 @app.get("/")
 def root():
